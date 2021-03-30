3b06e6117cb33451e84ca9dc757519972f04137a
Kal Kistner <36495202+JamesKistner@users.noreply.github.com>
2021-03-30 16:18:08 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import generateData
+import mysql.connector
+import pandas as pd
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Jameskis13!Kaleo432!",
+    database = "Cookies"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES;")
+tablesData = pd.DataFrame(mycursor.fetchall())
+tables = []
+
+for i in range(len(tablesData)):
+    tables.append(tablesData[0][i])
+
+print(tables)
+
+
+def printTable(tableName):
+    mycursor.execute("SELECT * FROM %s;", (tableName,))
+    data = mycursor.fetchall()
+    df = pd.DataFrame(data)
+    print(df)
+
+def addNewRecord(tableName):
+    print(1)
